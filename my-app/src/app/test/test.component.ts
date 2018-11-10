c775428e9b3d0bf20685d87be4489f0ba4a538d1
Shivakumar Parvathayya <shivz@Shivakumars-MacBook-Pro.local>
2018-11-10 18:56:54 -0500

    Commit

---
+++ my-app/src/app/test/test.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  title = 'Angular';
+  myId = 'testId';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  greetUser() {
+    return 'this is a method';
+  }
+}
